5f39f879d47bd0bcc5da7884ba63ce1d6a308993
VivithRK <vivithkanchi25@gmail.com>
2024-03-18 19:26:13 +0530

    added models, v1 routes

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("This is my server");
+const express = require("express");
+
+const app = express();
+
+// *middlewares
+// *routes
+// *error handler middlewares
+// *listen server
+
+const port = process.env.port || 9000;
+app.listen(port, console.log("Server is running on port " + port));
